6f904898e7514bc776366a62900b9dccfcc90b32
zax <appleapp2017@outlook.com>
2024-12-04 08:58:38 +0800

    线程池：multiprocessing解释

--- 基础篇/高性能异步爬虫/多任务协程01.py
+++ 基础篇/高性能异步爬虫/多任务协程01.py
@@ -1,33 +1,59 @@
+# import asyncio
+# import time
+
+
+# async def request(url):
+#     print('正在下载', url)
+#     # 在异步协程中，如果出现了同步模块相关的代码，就无法实现异步
+#     # time.sleep(2)
+#     # 当在asyncio中遇到阻塞操作必须进行手动挂起
+#     await asyncio.sleep(2)
+#     print('下载完成', url)
+
+
+# urls = [
+#     'www.baidu.com',
+#     'www.douban.com',
+#     'www.shu.edu.cn'
+# ]
+# # 任务列表：存放多个任务对象
+# tasks = []
+# for url in urls:
+#     c = request(url)
+#     task = asyncio.ensure_future(c)
+#     tasks.append(task)
+
+# start = time.time()
+
+# loop = asyncio.get_event_loop()
+# # 需要将任务列表封装到wait中
+# loop.run_until_complete(asyncio.wait(tasks))
+
+# end = time.time()
+# print(end - start)
+
+
 import asyncio
-import time
 
-
+# 定义异步函数
 async def request(url):
-    print('正在下载', url)
-    # 在异步协程中，如果出现了同步模块相关的代码，就无法实现异步
-    # time.sleep(2)
-    # 当在asyncio中遇到阻塞操作必须进行手动挂起
-    await asyncio.sleep(2)
-    print('下载完成', url)
+    print(f'正在下载 {url}')
+    await asyncio.sleep(2)  # 模拟异步阻塞操作
+    print(f'下载完成 {url}')
 
+# 主函数
+async def main():
+    urls = [
+        'www.baidu.com',
+        'www.douban.com',
+        'www.shu.edu.cn'
+    ]
+    
+    # 创建任务列表
+    tasks = [asyncio.create_task(request(url)) for url in urls]
+    
+    # 等待所有任务完成
+    await asyncio.gather(*tasks)
 
-urls = [
-    'www.baidu.com',
-    'www.douban.com',
-    'www.shu.edu.cn'
-]
-# 任务列表：存放多个任务对象
-tasks = []
-for url in urls:
-    c = request(url)
-    task = asyncio.ensure_future(c)
-    tasks.append(task)
-
-start = time.time()
-
-loop = asyncio.get_event_loop()
-# 需要将任务列表封装到wait中
-loop.run_until_complete(asyncio.wait(tasks))
-
-end = time.time()
-print(end - start)
+# 运行主函数
+asyncio.run(main())
--- 基础篇/高性能异步爬虫/线程池的基本使用.py
+++ 基础篇/高性能异步爬虫/线程池的基本使用.py
@@ -1,6 +1,19 @@
 import time
 from multiprocessing.dummy import Pool # 导入线程池模块对应的类
 
+'''
+线程池：multiprocessing.dummy 实际上是 threading 模块的一个包装器，它提供了与 multiprocessing 相同的接口，但使用的是线程而不是进程。
+适用场景：适用于 I/O 密集型任务，例如网络请求、文件读写等，因为这些任务主要等待外部资源的响应，使用线程可以在等待期间处理其他任务。
+全局解释器锁 (GIL)：由于使用的是线程，受限于 Python 的 GIL，多个线程不能同时执行 Python 字节码，因此在 CPU 密集型任务中效果不佳。
+multiprocessing
+进程池：multiprocessing 使用的是进程，每个进程都有自己的 Python 解释器和 GIL，因此可以真正并行地执行任务。
+适用场景：适用于 CPU 密集型任务，例如复杂计算、数据处理等，因为这些任务需要大量的 CPU 时间，使用多进程可以充分利用多核 CPU 的优势。
+开销：进程间通信和进程创建的开销比线程大，但在处理 CPU 密集型任务时，这些开销通常是值得的
+'''
+
+
+
+
 # # 使用单线程串行方式执行
 # def get_page(str):
 #     print('正在下载： ', str)
